21c3bc211a8e5176fe04a658cd922a5b669c24bf
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-04-30 20:31:59 -0400

    links

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,102 @@
+'use client'
+
 import Image from "next/image";
 import myImage from "./pic.png"
-import {AspectRatio, Box, Card, Typography} from "@mui/joy";
+import {
+  AspectRatio,
+  Box,
+  IconButton,
+  List, ListDivider,
+  ListItem, ListItemButton, ListItemContent, ListItemDecorator,
+  Snackbar,
+  Stack,
+  Typography
+} from "@mui/joy";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import KeyIcon from '@mui/icons-material/Key';
+import HomeIcon from '@mui/icons-material/Home';
+import LockPersonIcon from '@mui/icons-material/LockPerson';
+import BookIcon from '@mui/icons-material/Book';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import WorkIcon from '@mui/icons-material/Work';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import {useState} from "react";
+import {KeyboardArrowRight} from "@mui/icons-material";
+
+function PhotoCard() {
+  return <Stack useFlexGap direction='column' sx={{my: 'auto', width: 0.30, height: '100%', flex: '0 1 auto', py: 1}} spacing={2}>
+    <AspectRatio flex ratio="16/9" objectFit="cover" sx={{width: 1, height: 1, borderRadius: 5, flexBasis: 200}}>
+      <Image alt='Nathanael Gutierrez' src={myImage} placeholder="blur" />
+    </AspectRatio>
+    <Box sx={{borderRadius: 10, background: 'var(--joy-palette-primary-900)', px: 1, py: 0.5}}>
+      <Typography sx={{color: 'white'}} textAlign='center' level='h4'>Nathanael Gutierrez</Typography>
+      <Typography sx={{color: 'white'}} textAlign='center' level='title-md'>Software Engineer</Typography>
+    </Box>
+  </Stack>
+}
+
+function Socials() {
+  const [showSnackbar, setShowSnackbar] = useState(false)
+
+  const copyEmailToClipboard = () => {
+    navigator.clipboard.writeText('nathanaelg16@gmail.com')
+    setShowSnackbar(true)
+  }
+
+  return <>
+    <Stack sx={{mx: 'auto', mt: 'auto', mb: 2}} direction='row' spacing={2} useFlexGap justifyContent='start'>
+      <IconButton onClick={() => window.location.href = "https://github.com/nathanaelg16"} size='lg' sx={{'--IconButton-size': '4rem', '&:hover': {background: 'transparent'}, '&:active': {background: 'transparent'}}}>
+        <GitHubIcon />
+      </IconButton>
+      <IconButton onClick={copyEmailToClipboard} size='lg' sx={{'--IconButton-size': '4rem', '&:hover': {background: 'transparent'}, '&:active': {background: 'transparent'}}}>
+        <EmailIcon />
+      </IconButton>
+    </Stack>
+    <Snackbar open={showSnackbar} onClose={() => setShowSnackbar(false)} autoHideDuration={3000} anchorOrigin={{vertical: 'top', horizontal: 'center'}} startDecorator={<ContentCopyIcon />} color='success' variant='soft'>
+      Email copied to clipboard!
+    </Snackbar>
+  </>
+}
+
+function Link({title, startDecorator, endDecorator, onClick, disabled = false}) {
+  return <ListItem sx={{cursor: disabled ? 'not-allowed' : 'auto'}}>
+    <ListItemButton disabled={disabled} color='primary' onClick={disabled ? () => {} : onClick}>
+      <ListItemDecorator>{startDecorator}</ListItemDecorator>
+      <ListItemContent>{title}</ListItemContent>
+      {endDecorator ? endDecorator : <KeyboardArrowRight />}
+    </ListItemButton>
+  </ListItem>
+}
+
+function Links() {
+  return <Stack sx={{py: 2}} direction='row' spacing={5} justifyContent='space-between'>
+    <List size='lg' variant='outlined' color='neutral' sx={{borderRadius: 10, '--List-gap': '5px'}}>
+      <Link title='Experience' startDecorator={<WorkIcon />} />
+      <ListDivider/>
+      <Link title='Projects' startDecorator={<AssignmentIcon />} />
+    </List>
+    <List size='lg' variant='outlined' color='neutral' sx={{borderRadius: 10, '--List-gap': '5px'}}>
+      <Link title='Blog' startDecorator={<BookIcon />} disabled={true}/>
+      <ListDivider/>
+      <Link title='PGP Key' startDecorator={<KeyIcon />} onClick={() => window.location.href = '/nathanael-gutierrez.asc'}/>
+    </List>
+  </Stack>
+}
+
+function Content() {
+  return <Stack sx={{height: 1, flex: '1', px: 7, pt: 2, pr: 4}}>
+    <Links />
+    <Socials />
+  </Stack>
+}
+
 
 export default function Home() {
-  return <>
-    <Box sx={{display: 'flex', width: 1, py: 20}}>
-      <Card orientation='horizontal' sx={{mx: 'auto', width: 0.4}}>
-        <AspectRatio ratio="1" objectFit="cover">
-          <Image alt='Nathanael Gutierrez' src={myImage} placeholder="blur" />
-        </AspectRatio>
-        <Typography level='h2'>Nathanael Gutierrez</Typography>
-      </Card>
-    </Box>
-  </>
+  return <Box sx={{display: 'flex', width: '100svw', height: '100svh', userSelect: 'none'}}>
+    <Stack direction='row' justifyContent='space-between' sx={{mx: 'auto', border: '2px solid var(--joy-palette-primary-900)', background: 'var(--joy-palette-primary-50)', borderRadius: 20, width: 0.75, height: 0.60, my: 10, px: 2}}>
+      <PhotoCard />
+      <Content />
+    </Stack>
+  </Box>
 }
