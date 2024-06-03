5cc47c7bd62dd340584a131b0a2d8bb851623f99
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-06-03 14:07:25 -0400

    updating reading log

--- src/app/terminal.js
+++ src/app/terminal.js
@@ -35,19 +35,19 @@ const fileSystem = [new Node('files', 'dir', [
     new Node('reading-list.txt', 'file', <>
         <p># Currently Reading</p>
         <List className={`${FiraCode.className} ${styles.list}`} component="ol" marker="decimal">
-            <Link title='Drown by Junot Diaz' url='https://a.co/d/04hzq1M'/>
+            <Link title='The Brief Wondrous Life of Oscar Wao by Junot Diaz' url='https://a.co/d/2lmliwv'/>
             <Link title='The Prince and the Pauper by Mark Twain (Mandarin edition)' url='https://a.co/d/grYhpua'/>
         </List>
         <br/>
         <p># Want to Read</p>
         <List className={`${FiraCode.className} ${styles.list}`} component="ol" marker="decimal">
-            <Link title='The Brief Wondrous Life of Oscar Wao by Junot Diaz' url='https://a.co/d/2lmliwv'/>
             <Link title='The Sparrow by Mary Doria Russell' url='https://a.co/d/2uEkxDl'/>
             <Link title='Everyone Who Is Gone Is Here by Jonathan Blitzer' url='https://a.co/d/eQCoo7U'/>
         </List>
         <br/>
         <p># Previously Read</p>
         <List className={`${FiraCode.className} ${styles.list}`} component="ol" marker="decimal">
+            <Link title='Drown by Junot Diaz' url='https://a.co/d/04hzq1M'/>
             <Link title='Cultish: The Language of Fanaticism by Amanda Montell' url='https://a.co/d/6juJkWG'/>
             <Link title='Mad Honey by Jodi Picoult' url='https://a.co/d/6FxKcp7'/>
         </List>
